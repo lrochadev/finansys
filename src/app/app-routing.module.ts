c59c073ce4e6e376f1b7733ef765cc25a9cdd4f7
Leonardo Rocha <lrochadev@gmail.com>
2019-02-05 00:04:21 -0200

    Navbar; Categories module; in-memory-database; Category model, Category list/form Component, CategoryService

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
