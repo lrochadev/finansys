e15aafab0d27e070b7af5bdefdf048e8f7737d0f
Leonardo Rocha <lrochadev@gmail.com>
2019-02-05 00:12:34 -0200

    Adicinando o interceptor para angular-in-memory-web-api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InMemoryDatabase } from './in-memory-database';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
